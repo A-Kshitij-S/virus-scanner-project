74dd77e9a851e8cf91bbf6738837ce6ce5505013
A-Kshitij-S <ankurkshitijsahai22@gmail.com>
2025-07-17 20:34:11 +0530

    debugging

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -74,8 +74,8 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: true,
-        sameSite: "None",
+        secure: false,
+        sameSite: "Lax",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(200)
